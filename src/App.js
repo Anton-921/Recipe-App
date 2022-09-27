c109b0bf5fefc082a90e00c3d246eb979eb95087
yeffry <yeffry45@gmail.com>
2022-09-27 14:20:53 -0400

    reflect on testing changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Header from "./components/Header/Header";
 import styles from "./App.module.css";
 import Welcome from "./pages/Welcome/Welcome";
 import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Feed from "./pages/Feed/Feed";
 
 const App = () => {
   return (
-    <div className={styles.App}>
+    <main className={styles.App}>
       <Header /> {/* Constant*/}
-      <Welcome />          {/* Dynamyc */}
+      <Feed />
       <Footer /> {/* Constant*/}
-    </div>
+    </main>
   );
 };
 
