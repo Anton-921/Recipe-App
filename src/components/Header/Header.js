0cf20ff7af6b8a8dc731e777cae871f5fad3e0fd
yeffry <yeffry45@gmail.com>
2022-09-29 11:57:08 -0400

    rework header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,35 +2,51 @@ import React, { useState } from "react";
 import logo from "../../diet.png";
 import styles from "../Header/Header.module.css";
 import { AiOutlineMenu } from "react-icons/ai";
+import { MdOutlineClose } from "react-icons/md";
 
 const Header = () => {
   const [user, setUser] = useState("Yeffry");
+  const [checked, setChecked] = useState(false);
+
+  console.log(checked);
   return (
     <header className={styles.header}>
-      {/* Website Logo */}
-      <div className={styles.logo}>
-        <img src={logo} alt="food logo" />
-      </div>
-
-      {/* If user is not signed in shows buttons*/}
-      {!user && (
-        <div className={styles["btn-container"]}>
-          <button className={[styles["btn-main"], styles.btn].join(" ")}>
-            Login
-          </button>
-          <button className={[styles["btn-minor"], styles.btn].join(" ")}>
-            Sign-up
-          </button>
-        </div>
-      )}
-      {/* If user is signed in then show hamburger icon */}
-      {user && (
-        <div className={styles.hamburger}>
-          <AiOutlineMenu size={30} />
-        </div>
-      )}
+      <nav className={styles.nav}>
+        <input
+          type="checkbox"
+          id="nav__checkbox"
+          className={styles["nav__checkbox"]}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        <label htmlFor="nav__checkbox" className={styles["nav__toggle"]}>
+          <AiOutlineMenu className={styles.menu} size={35} />
+          <MdOutlineClose className={styles.close} size={35}/>
+        </label>
+        <ul className={styles["nav__menu"]}>
+          <li>
+            <a href="#">
+              <img src={logo} className={styles.logo} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Work</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
 
 export default Header;
+
+// Icon always displayed
+// User signed in? Display hamburger menu
+// User is not signed in? Display Button menu
+
