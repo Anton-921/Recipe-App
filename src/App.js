f36a0a8b7e42d732507cee15921a06695715da9c
yeffry <yeffry45@gmail.com>
2022-09-29 14:26:54 -0400

    add public routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import styles from "./App.module.css";
 import Welcome from "./pages/Welcome/Welcome";
@@ -5,13 +6,33 @@ import Footer from "./components/Footer/Footer";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Feed from "./pages/Feed/Feed";
-
+import AddRecipe from "./pages/AddRecipe/AddRecipe";
+import WelcomeHeader from "./components/WelcomeHeader/WelcomeHeader";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  const logIn = () => {
+    setIsLoggedIn(true);
+  };
+  const logOut = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <main className={styles.App}>
-      <Header /> {/* Constant*/}
-      <Feed />
-      <Footer /> {/* Constant*/}
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<WelcomeHeader onLogIn={logIn} />}>
+            <Route index element={<Welcome />} />
+            <Route path="signin" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+
+          {/* Protected Routes */}
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 };
