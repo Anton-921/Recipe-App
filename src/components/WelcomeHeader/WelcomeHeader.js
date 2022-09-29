d50afc273d7146f32ec835148fea0c8108b666f6
yeffry <yeffry45@gmail.com>
2022-09-29 14:27:18 -0400

    add Outlet component

--- src/components/WelcomeHeader/WelcomeHeader.js
+++ src/components/WelcomeHeader/WelcomeHeader.js
@@ -1,25 +1,33 @@
 import React from "react";
 import styles from "../WelcomeHeader/WelcomeHeader.module.css";
 import logo from "../../diet.png";
+import { Outlet } from "react-router-dom";
 
-const WelcomeHeader = () => {
+const WelcomeHeader = ({ onLogIn }) => {
   return (
-    <header className={styles.header}>
-      <div className={styles.layout}>
-        <div className={styles.logo}>
-          <img src={logo} alt="food logo" />
-        </div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.layout}>
+          <div className={styles.logo}>
+            <img src={logo} alt="food logo" />
+          </div>
 
-        <div className={styles["btn-container"]}>
-          <button className={[styles["btn-main"], styles.btn].join(" ")}>
-            Login
-          </button>
-          <button className={[styles["btn-minor"], styles.btn].join(" ")}>
-            Sign-up
-          </button>
+          <div className={styles["btn-container"]}>
+            <button
+              className={[styles["btn-main"], styles.btn].join(" ")}
+              onClick={() => onLogIn()}
+            >
+              Login
+            </button>
+            <button className={[styles["btn-minor"], styles.btn].join(" ")}>
+              Sign-up
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <Outlet />
+    </div>
   );
 };
 
