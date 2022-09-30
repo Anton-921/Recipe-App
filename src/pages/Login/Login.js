2ab1bf41da7259763f559d96c52472de5e94edf2
yeffry <yeffry45@gmail.com>
2022-09-30 14:57:48 -0400

    add basic login simulation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,20 @@
-import React from "react";
-import styles from '../Login/Login.module.css'
+import React, { useContext } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import AuthContext from "../../context/AuthProvider";
+import styles from "../Login/Login.module.css";
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/home";
+
+  console.log(from)
+  const handleLogin = () => {
+    setAuth({ user: "Yeffry" });
+    navigate(from, { replace: true });
+  };
+
   return (
     <section className={styles.container}>
       <h1>Login</h1>
@@ -16,7 +29,12 @@ const Login = () => {
         </div>
 
         <div>
-          <button className={`${styles.btn} ${styles['btn-main']}`}>Sign In</button>
+          <button
+            className={`${styles.btn} ${styles["btn-main"]}`}
+            onClick={handleLogin}
+          >
+            Sign In
+          </button>
         </div>
       </form>
     </section>
