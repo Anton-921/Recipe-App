5d15670e604062ed53d4f5b6673b88b24eb31a1f
yeffry <yeffry45@gmail.com>
2022-10-03 12:32:44 -0400

    add more protected routes + remove unused components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import { useState, useContext } from "react";
-import Header from "./components/Header/Header";
-import styles from "./App.module.css";
 import Welcome from "./pages/Welcome/Welcome";
-import Footer from "./components/Footer/Footer";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Feed from "./pages/Feed/Feed";
 import AddRecipe from "./pages/AddRecipe/AddRecipe";
-import Dashboard from "./components/Dashboard/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RequireAuth from "./components/Auth/RequireAuth";
-import useAuth from "./hooks/useAuth";
 import Layout from "./components/Layout/Layout";
+import MyRecipe from "./pages/MyRecipe/MyRecipe";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(null);
-
   return (
     // <main className={styles.App}>
     <BrowserRouter>
@@ -28,9 +21,11 @@ const App = () => {
 
           {/* Protected Routes */}
           <Route element={<RequireAuth />}>
-            <Route path="/home" element={<Feed />} />
+            <Route path="home" element={<Feed />} />
+            <Route path="addrecipe" element={<AddRecipe />} />
+            <Route path="myrecipe" element={<MyRecipe />} />
           </Route>
-        </Route> 
+        </Route>
       </Routes>
     </BrowserRouter>
     // </main>
