753ac6a5a7aa87423244376ebb942faa50c627f0
yeffry <yeffry45@gmail.com>
2022-10-03 14:21:38 -0400

    remove deprecated component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import styles from "../../App.module.css";
 import AuthContext from "../../context/AuthProvider";
-import Dashboard from "../Dashboard/Dashboard";
 import Header from "../Header/Header";
 
 const Layout = () => {
