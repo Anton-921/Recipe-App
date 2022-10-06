813e909415f3cd9ce154dd7c956c17bc606b465b
yeffry <yeffry45@gmail.com>
2022-10-06 15:33:06 -0400

    add state to keep track of user input

--- src/pages/AddRecipe/AddRecipe.js
+++ src/pages/AddRecipe/AddRecipe.js
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "../AddRecipe/AddRecipe.module.css";
+import { useNavigate } from "react-router-dom";
+import RecipeContext from "../../context/RecipeProvider";
 
-const AddRecipe = () => {
+const AddRecipe = ({ setRecipes }) => {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [time, setTime] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [description, setDescription] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [directions, setDirections] = useState("");
+  const [image, setImage] = useState("");
+  let navigate = useNavigate();
+
+  const handleAddRecipe = (e) => {
+    e.preventDefault();
+    const recipe = {
+      name,
+      type,
+      time,
+      difficulty,
+      description,
+      ingredients,
+      directions,
+      image,
+    };
+    // post request with recipe
+    setRecipes((recipes) => {
+      return [recipe, ...recipes]
+    })
+
+    navigate("/home");
+  };
+  const imgFilehandler = (e) => {
+    if (e.target.files.length !== 0) {
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+  console.log(image);
+
   return (
     <section className={styles.container}>
-      <form action="" className={styles.form}>
+      <form action="" className={styles.form} onSubmit={handleAddRecipe}>
         <h1>Add Recipe</h1>
         <div className={styles["form-control"]}>
           <label htmlFor="recipe">Recipe Name</label>
-          <input type="text" name="recipe" id="recipe" />
+          <input
+            type="text"
+            name="recipe"
+            maxLength={30}
+            id="recipe"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
         </div>
 
         <div className={styles["form-control"]}>
           <label htmlFor="type">Recipe Type</label>
-          <select name="type" id="type">
+          <select
+            name="type"
+            id="type"
+            onChange={(e) => setType(e.target.value)}
+            value={type}
+          >
+            <option>Choose Recipe Type</option>
             <option value="breakfast">Breakfast</option>
             <option value="lunch">Lunch</option>
             <option value="dinner">Dinner</option>
@@ -23,7 +74,13 @@ const AddRecipe = () => {
 
         <div className={styles["form-control"]}>
           <label htmlFor="time">Estimated Time</label>
-          <select name="time" id="time">
+          <select
+            name="time"
+            id="time"
+            onChange={(e) => setTime(e.target.value)}
+            value={time}
+          >
+            <option>Choose Estimate Time</option>
             <option value="15 min">15 Min</option>
             <option value="30 min">30 Min</option>
             <option value="45 min">45 Min</option>
@@ -33,11 +90,26 @@ const AddRecipe = () => {
 
         <div className={styles["form-control"]}>
           <label htmlFor="difficulty">Difficulty</label>
-          <select name="difficulty" id="difficulty">
+          <select
+            name="difficulty"
+            id="difficulty"
+            onChange={(e) => setDifficulty(e.target.value)}
+            value={difficulty}
+          >
+            <option>Choose Difficulty</option>
             <option value="beginner">Beginner</option>
             <option value="intermediate">Intermediate</option>
             <option value="advanced">Advanced</option>
           </select>
+        </div>
+
+        <div className={styles["form-control"]}>
+          <label htmlFor="difficulty">Recipe Image</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => imgFilehandler(e)}
+          />
         </div>
 
         <div className={styles["form-control"]}>
@@ -46,7 +118,11 @@ const AddRecipe = () => {
             name="description"
             id="description"
             cols="25"
-            rows="5"
+            rows="1"
+            maxLength={500}
+            wrap="hard"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           ></textarea>
         </div>
 
@@ -56,8 +132,12 @@ const AddRecipe = () => {
             name="ingredients"
             id="ingredients"
             cols="25"
-            rows="4"
+            rows="3"
+            maxLength={500}
+            wrap="hard"
             placeholder="Ingredient 1, Ingredient 2, ..."
+            value={ingredients}
+            onChange={(e) => setIngredients(e.target.value)}
           ></textarea>
         </div>
 
@@ -73,6 +153,8 @@ const AddRecipe = () => {
             Direction 2 
             ...
             "
+            value={directions}
+            onChange={(e) => setDirections(e.target.value)}
           ></textarea>
         </div>
         <div>
