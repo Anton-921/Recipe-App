cfee5fcc930bc6b74ea0c9900d3ea63446b241cb
yeffry <yeffry45@gmail.com>
2022-10-09 17:24:27 -0400

    add POST route logic for recipes

--- server/server.js
+++ server/server.js
@@ -18,6 +18,29 @@ app.get("/api/recipes", async (req, res) => {
   res.json(recipes);
 });
 
+app.post("/api/recipes", upload.single("imageFile"), async (req, res) => {
+  const body = req.body;
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    const recipe = await Recipe.create({
+      name: body.recipeName,
+      type: body.recipeType,
+      time: body.estimatedTime,
+      difficulty: body.difficulty,
+      description: body.description,
+      ingredients: body.ingredients,
+      directions: body.directions,
+      image: result.secure_url,
+      cloudinaryId: result.public_id,
+    });
+
+    res.status(200).json({ recipe });
+  } catch (error) {
+    console.log(console.log(error));
+  }
+});
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
