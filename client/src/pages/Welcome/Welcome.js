2abd0e103c08c21e61f750fd1ea1d92c09214235
yeffry <yeffry45@gmail.com>
2022-10-10 15:40:35 -0400

    add a more responsive layout to component

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -5,21 +5,26 @@ import welcomeSvg from "../../welcome.svg";
 const Welcome = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.icon}>
-        <img src={welcomeSvg} className={styles.svg} alt="people eating" />
+      <div className={styles["content-wrapper"]}>
+        <div className={styles.icon}>
+          <img src={welcomeSvg} className={styles.svg} alt="people eating" />
+        </div>
+
+        <div className={styles.content}>
+          <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
+            reprehenderit ullam cupiditate possimus laborum nobis sit corrupti
+            quaerat consectetur sapiente, itaque eveniet ratione perspiciatis
+            minima.
+          </p>
+
+          <button className={[styles["btn-main"], styles.btn].join(" ")}>
+            Sign up for Res!
+          </button>
+        </div>
       </div>
-      <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-        reprehenderit ullam cupiditate possimus laborum nobis sit corrupti
-        quaerat consectetur sapiente, itaque eveniet ratione perspiciatis
-        minima.
-      </p>
-
-      <button className={[styles["btn-main"], styles.btn].join(" ")}>
-        Sign up for Res!
-      </button>
     </section>
   );
 };
