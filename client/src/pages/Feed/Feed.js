3c2dcc47e8ce1c748429d9fb077da7ccedefdc4f
yeffry <yeffry45@gmail.com>
2022-10-10 15:38:03 -0400

    add data fetching on component load

--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 import styles from "../Feed/Feed.module.css";
 import Cards from "../../components/Cards/Card";
+import { useRecipes } from "../../context/RecipeProvider";
 
-const Feed = ({ recipes }) => {
+const Feed = () => {
+  const { recipes, setRecipes } = useRecipes();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/recipes")
+      .then((res) => res.json())
+      .then((data) => {
+        setRecipes(data);
+      });
+  }, [setRecipes]);
+
   return (
     <section className={styles.container}>
       <div className={styles.feed}>
         <h1>Recipes Feed</h1>
-        <Cards recipes={recipes} />
+        {recipes ? <Cards items={recipes} /> : <p>Loading</p>}
       </div>
     </section>
   );
