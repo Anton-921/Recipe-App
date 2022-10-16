b2af9a60d54cf6e9f9de0a8e7efc23b19efd4b3d
yeffry <yeffry45@gmail.com>
2022-10-16 18:28:53 -0400

    add basic signup http request

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import styles from "../Login/Login.module.css";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      pwd,
+    };
+
+    fetch("http://localhost:8080/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+  };
+
   return (
     <section className={styles.container}>
       <h1>Sign Up</h1>
-      <form action="" className={styles.form}>
-        <div className={styles["form-control"]}>
+      <form onSubmit={handleSignup} className={styles.form}>
+        {/* <div className={styles["form-control"]}>
           <label htmlFor="username">Username</label>
           <input type="text" name="username" />
-        </div>
+        </div> */}
 
         <div className={styles["form-control"]}>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
         </div>
 
-        <div className={styles["form-control"]}>
+        {/* <div className={styles["form-control"]}>
           <label htmlFor="password-confirm">Confirm Password</label>
           <input type="password" name="password-confirm" />
-        </div>
+        </div> */}
 
         <div>
           <button className={`${styles.btn} ${styles["btn-main"]}`}>
