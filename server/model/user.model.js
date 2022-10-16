6a757dcbaeec88823c0782fd1a7a494aa5c5e1d8
yeffry <yeffry45@gmail.com>
2022-10-16 16:51:53 -0400

    add User schema + model

---
+++ server/model/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = User = mongoose.model('User', UserSchema)
