e3ee8a0b68e52e774dd9638a62d1cf3bfd712d93
yeffry <yeffry45@gmail.com>
2022-10-16 16:48:48 -0400

    remove dummy data from components

--- client/src/App.js
+++ client/src/App.js
@@ -7,79 +7,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RequireAuth from "./components/Auth/RequireAuth";
 import Layout from "./components/Layout/Layout";
 import MyRecipe from "./pages/MyRecipe/MyRecipe";
-import { useState } from "react";
-import oatmeal from "./oatmeal.jpg";
 
-const items = [
-  {
-    id: 1,
-    image: oatmeal,
-    name: "Chai Oatmeal",
-    type: "Breakfast",
-    time: "15 Min",
-    difficulty: "Beginner",
-    likes: "742",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error veniam quidem pariatur? Optio mollitia ullam nisi hic. Nostrum, ea vero!",
-    ingredients:
-      "Ingredients: Milk, salt, coriander, cardamom, cinnamon, turmeric, honey, vanilla extract, regular oats, oat bran.",
-  },
-  {
-    id: 2,
-    image: oatmeal,
-    name: "Chai Oatmeal",
-    type: "Breakfast",
-    time: "15 Min",
-    difficulty: "Beginner",
-    likes: "742",
-    description: "Lorem ipsum, a ullam nisi hic. Nostrum, ea vero!",
-    ingredients:
-      "Ingredients: Milk, salt, coriander, cardamom, cinnamon, turmeric, honey, vanilla extract, regular oats, oat bran.",
-  },
-
-  {
-    id: 3,
-    image: oatmeal,
-    name: "Chai Oatmeal",
-    type: "Breakfast",
-    time: "15 Min",
-    difficulty: "Beginner",
-    likes: "742",
-    description:
-      "Lorem ipsum, dolor sitctetur adipisicing elit. Error veniam quidem pariatur? Optio mollitia ullam nisi hic. Nostrum, ea vero!",
-    ingredients:
-      "Ingredients: Milk, salt, coriander, cardamom, cinnamon, turmeric, honey, vanilla extract, regular oats, oat bran.",
-  },
-  {
-    id: 4,
-    image: oatmeal,
-    name: "Chai Oatmeal",
-    type: "Breakfast",
-    time: "15 Min",
-    difficulty: "Beginner",
-    likes: "742",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error veniam quidem pariatur? Optio mollitia ullam nisi hic. Nostrum, ea vero!",
-    ingredients:
-      "Ingredients: Milk, salt, coriander, cardamom, cinnamon, turmeric, honey, vanilla extract, regular oats, oat bran.",
-  },
-  {
-    id: 5,
-    image: oatmeal,
-    name: "Chai Oatmeal",
-    type: "Breakfast",
-    time: "15 Min",
-    difficulty: "Beginner",
-    likes: "742",
-    description:
-      "Lorem ipsum, dolor sit amet conse pariatur? Optio mollitia ullam nisi hic. Nostrum, ea vero!",
-    ingredients:
-      "Ingredients: Milk, salt, coria, cinnamon, turmeric, honey, vanilla extract, regular oats, oat bran.",
-  },
-];
 const App = () => {
-  const [recipes, setRecipes] = useState(items);
-
   return (
     // <main className={styles.App}>
     <BrowserRouter>
@@ -92,11 +21,8 @@ const App = () => {
 
           {/* Protected Routes */}
           <Route element={<RequireAuth />}>
-            <Route path="home" element={<Feed recipes={recipes} />} />
-            <Route
-              path="addrecipe"
-              element={<AddRecipe setRecipes={setRecipes} recipes={recipes} />}
-            />
+            <Route path="home" element={<Feed />} />
+            <Route path="addrecipe" element={<AddRecipe />} />
             <Route path="myrecipe" element={<MyRecipe />} />
           </Route>
         </Route>
