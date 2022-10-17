233f53079156a524bbf23ecafc20273de9cbf4af
yeffry <yeffry45@gmail.com>
2022-10-17 18:32:01 -0400

    add route logic to handle login process

--- server/server.js
+++ server/server.js
@@ -3,15 +3,12 @@ const cors = require("cors");
 const app = express();
 const Recipe = require("./model/recipe.model");
 const User = require("./model/user.model");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const connectDB = require("./config/db.config");
 const cloudinary = require("./middleware/cloudinary");
 const upload = require("./middleware/multer");
-const cookieSession = require("cookie-session");
+const session = require("express-session");
 require("dotenv").config({ path: "./config/.env" });
-
-// DB users most likely mongo for now
-const Users = [];
 
 connectDB();
 
@@ -19,14 +16,44 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cookieSession({
-    keys: ["ldfg3ldfgdf4l3"],
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
   })
 );
 
 app.get("/api/recipes", async (req, res) => {
   const recipes = await Recipe.find({});
   res.json(recipes);
+});
+
+app.post("/api/signout", (req, res) => {
+  res.json({ message: "You are signed out" });
+});
+
+app.post("/api/signin", async (req, res) => {
+  const { email, pwd } = req.body;
+
+  if (!email || !pwd) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+  }
+  const foundUser = await User.findOne({ email });
+  if (!foundUser) return res.sendStatus(401); // Unauthorized
+
+  const passwordMatch = await bcrypt.compare(pwd, foundUser.password);
+
+  if (passwordMatch) {
+    res.json({ message: "hello" });
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 app.post("/api/register", async (req, res) => {
@@ -39,20 +66,21 @@ app.post("/api/register", async (req, res) => {
   // check for user duplicates
   const duplicate = await User.findOne({ email });
   if (duplicate) return res.sendStatus(409); // conflict
-  
+
   try {
     const hashedPwd = await bcrypt.hash(pwd, 10);
     const newUser = await User.create({
       email: email,
       password: hashedPwd,
     });
-    console.log(newUser);
-    req.session.userId = newUser._id
     // Added by cookie seession
-    res.status(201).json({ success: `New user ${email} created!`, userId: `${req.session.userId}` });
+    res.status(201).json({
+      success: `New user ${email} created!`,
+      userId: `${req.session.userId}`,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
-    console.log(error)
+    console.log(error);
   }
 });
 
