61b78b99cf98d50fb57556c1e13cd8eeb024f10f
yeffry <yeffry45@gmail.com>
2022-10-19 23:31:55 -0400

    add main app configs

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const authRoutes = require("./routes/auth");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config({
+  path: path.join(__dirname, ".env"),
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(
+  session({
+    name: "yeffry.sid",
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: "none",
+    },
+  })
+);
+app.use("/api/v1/auth", authRoutes);
+
+module.exports = app;
