408bc0ec426a52e610a91662db56ac4fd7b0b068
yeffry <yeffry45@gmail.com>
2022-10-21 16:10:48 -0400

    add conditional based on loading state from auth

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,22 @@
-import { useContext } from "react";
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import styles from "../../App.module.css";
 import AuthContext from "../../context/AuthProvider";
 import Header from "../Header/Header";
+import Placeholder from "../Placeholder/Placeholder";
+import Spinner from "../Spinner/Spinner";
 
-const Layout = () => {
-  const { auth } = useContext(AuthContext);
+const Layout = ({ loading }) => {
   return (
     <main className={styles.App}>
-      <Header />
-      <Outlet />
+      {!loading ? (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ) : (
+        <Spinner />
+      )}
     </main>
   );
 };
