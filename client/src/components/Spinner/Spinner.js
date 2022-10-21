bc179ff383935511b1c48067ff259df3d8763a5c
yeffry <yeffry45@gmail.com>
2022-10-21 16:11:32 -0400

    add initial styles

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import styles from "../Spinner/Spinner.module.css";
+
+const Spinner = () => (
+  <div className={styles.container}>
+    <div className={styles.loader}></div>
+  </div>
+);
+
+export default Spinner;
