62d8804148319369213ed12ccf6c91de52d9c213
yeffry <yeffry45@gmail.com>
2022-10-20 23:57:49 -0400

    get user on component mount

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,54 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Feed from "./pages/Feed/Feed";
 import AddRecipe from "./pages/AddRecipe/AddRecipe";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import RequireAuth from "./components/Auth/RequireAuth";
 import Layout from "./components/Layout/Layout";
 import MyRecipe from "./pages/MyRecipe/MyRecipe";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "./context/AuthProvider";
 
 const App = () => {
-  return (
-    // <main className={styles.App}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Public Routes */}
-          <Route index element={<Welcome />} />
-          <Route path="signin" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+  const [isLoading, setIsLoading] = useState(false);
+  const { setAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-          {/* Protected Routes */}
-          <Route element={<RequireAuth />}>
-            <Route path="home" element={<Feed />} />
-            <Route path="addrecipe" element={<AddRecipe />} />
-            <Route path="myrecipe" element={<MyRecipe />} />
-          </Route>
+  useEffect(() => {
+    getLoggedUser();
+  }, [setAuth]);
+
+  const getLoggedUser = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/v1/auth/profile",
+        {
+          credentials: "include",
+        }
+      );
+      const resJson = await response.json();
+      setAuth(resJson.user);
+      navigate("/home");
+    } catch (error) {
+      navigate("/signin");
+    }
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout isLoading={isLoading}/>}>
+        {/* Public Routes */}
+        <Route index element={<Welcome />} />
+        <Route path="signin" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+
+        {/* Protected Routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="home" element={<Feed />} />
+          <Route path="addrecipe" element={<AddRecipe />} />
+          <Route path="myrecipe" element={<MyRecipe />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-    // </main>
+      </Route>
+    </Routes>
   );
 };
 
