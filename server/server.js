29c013f9905701365628824acbfee7095c7766b8
yeffry <yeffry45@gmail.com>
2022-10-21 17:44:51 -0400

    add server config

--- server/server.js
+++ server/server.js
@@ -1,114 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const Recipe = require("./model/recipe.model");
-const User = require("./model/user.model");
-const bcrypt = require("bcrypt");
-const connectDB = require("./config/db.config");
-const cloudinary = require("./middleware/cloudinary");
-const upload = require("./middleware/multer");
-const session = require("express-session");
-require("dotenv").config({ path: "./config/.env" });
+const path = require("path");
+const dotenv = require("dotenv");
+const app = require("./app");
+// const { PORT, DATABASE_CONNECTION_STRINGS } = require("./config");
+const mongoose = require('mongoose')
 
-connectDB();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-  })
-);
-
-app.get("/api/recipes", async (req, res) => {
-  const recipes = await Recipe.find({});
-  res.json(recipes);
+dotenv.config({
+  path: path.join(__dirname, ".env"),
 });
 
-app.post("/api/signout", (req, res) => {
-  res.json({ message: "You are signed out" });
-});
-
-app.post("/api/signin", async (req, res) => {
-  const { email, pwd } = req.body;
-
-  if (!email || !pwd) {
-    return res
-      .status(400)
-      .json({ message: "Username and password are required" });
-  }
-  const foundUser = await User.findOne({ email });
-  if (!foundUser) return res.sendStatus(401); // Unauthorized
-
-  const passwordMatch = await bcrypt.compare(pwd, foundUser.password);
-
-  if (passwordMatch) {
-    res.json({ message: "hello" });
-  } else {
-    res.sendStatus(401);
-  }
-});
-
-app.post("/api/register", async (req, res) => {
-  const { email, pwd } = req.body;
-  if (!email || !pwd) {
-    return res
-      .status(400)
-      .json({ message: "Username and password are required" });
-  }
-  // check for user duplicates
-  const duplicate = await User.findOne({ email });
-  if (duplicate) return res.sendStatus(409); // conflict
-
+const connectDB = async () => {
   try {
-    const hashedPwd = await bcrypt.hash(pwd, 10);
-    const newUser = await User.create({
-      email: email,
-      password: hashedPwd,
+    const conn = await mongoose.connect(process.env.DATABASE_CONNECTION_STRINGS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    // Added by cookie seession
-    res.status(201).json({
-      success: `New user ${email} created!`,
-      userId: `${req.session.userId}`,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.log(error);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
-});
+};
 
-app.post("/api/recipes", upload.single("imageFile"), async (req, res) => {
-  const body = req.body;
-  try {
-    const result = await cloudinary.uploader.upload(req.file.path);
+connectDB()
 
-    const recipe = await Recipe.create({
-      name: body.recipeName,
-      type: body.recipeType,
-      time: body.estimatedTime,
-      difficulty: body.difficulty,
-      description: body.description,
-      ingredients: body.ingredients,
-      directions: body.directions,
-      image: result.secure_url,
-      cloudinaryId: result.public_id,
-    });
-
-    res.status(200).json({ recipe });
-  } catch (error) {
-    console.log(console.log(error));
-  }
-});
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server Started listening on port ${process.env.PORT}`);
 });
