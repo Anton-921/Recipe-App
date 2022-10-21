cca8e45f32fe483f2acca79c1dbf7edfcf341e49
yeffry <yeffry45@gmail.com>
2022-10-20 23:58:30 -0400

    move part of react-router to index

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { RecipeProvider } from "./context/RecipeProvider";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <RecipeProvider>
-        <App />
-      </RecipeProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <RecipeProvider>
+          <App />
+        </RecipeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
