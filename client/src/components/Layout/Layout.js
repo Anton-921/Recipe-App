0e622d074d95b7bf588e17f53cbb6de22d5ab710
yeffry <yeffry45@gmail.com>
2022-10-21 19:05:49 -0400

    remove unneeded imports

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,9 +1,6 @@
-import { useContext, useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import styles from "../../App.module.css";
-import AuthContext from "../../context/AuthProvider";
 import Header from "../Header/Header";
-import Placeholder from "../Placeholder/Placeholder";
 import Spinner from "../Spinner/Spinner";
 
 const Layout = ({ loading }) => {
