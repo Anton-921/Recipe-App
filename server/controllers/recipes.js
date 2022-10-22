10b52a80433cd58174f5a89071256e716819ef1a
yeffry <yeffry45@gmail.com>
2022-10-22 14:25:16 -0400

    add recipes controller

---
+++ server/controllers/recipes.js
@@ -0,0 +1,36 @@
+const Recipe = require("../models/recipe");
+const cloudinary = require("../middleware/cloudinary");
+
+module.exports = {
+  getRecipes: async (req, res) => {
+    try {
+      const recipes = await Recipe.find({});
+      res.json(recipes);
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  addRecipe: async (req, res) => {
+    const body = req.body;
+    try {
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      const recipe = await Recipe.create({
+        name: body.recipeName,
+        type: body.recipeType,
+        time: body.estimatedTime,
+        difficulty: body.difficulty,
+        description: body.description,
+        ingredients: body.ingredients,
+        directions: body.directions,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+      });
+
+      res.status(200).json({ recipe });
+    } catch (error) {
+      console.log(console.log(error));
+    }
+  },
+};
