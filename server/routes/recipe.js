bb1281fff3fddb6f5497702746eb681d60f9e777
yeffry <yeffry45@gmail.com>
2022-10-26 19:00:38 -0400

    add likeRecipe route

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -5,11 +5,12 @@ const {
   addRecipe,
   getRecipes,
   getUserRecipes,
+  likeRecipe,
 } = require("../controllers/recipes");
 const upload = require("../middleware/multer");
 
 router.get("/getRecipes", getRecipes);
 router.post("/postRecipe", upload.single("imageFile"), addRecipe);
 router.get("/getUserRecipes", loginRequired, getUserRecipes);
-
+router.post('/likeRecipe/:id', likeRecipe)
 module.exports = router;
