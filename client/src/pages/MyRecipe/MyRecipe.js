b6eb1044a6bffd1a8c5103de23cd9912ecbb725a
yeffry <yeffry45@gmail.com>
2022-10-26 11:01:21 -0400

    add logic to get logged user recipes

--- client/src/pages/MyRecipe/MyRecipe.js
+++ client/src/pages/MyRecipe/MyRecipe.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../MyRecipe/MyRecipe.module.css";
 import Cards from "../../components/Cards/Card";
 
 const MyRecipe = () => {
-  // When we come to this page through routing
-  // Step 1-Grab ID
-  // Step 2-On Mount get all recipes from my profile
-  // Step 3-Send them down to Recipes component which renders a list of them
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    fetchUserRecipes()
+  }, []);
+
+  const fetchUserRecipes = async () => {
+    const response = await fetch(
+      "http://localhost:8080/api/v1/recipes/getUserRecipes",
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const resJson = await response.json()
+    if (response.status === 200) {
+      setRecipes(resJson.recipes)
+    }
+  };
   return (
     <section className={styles.container}>
       <div className={styles.recipes}>
         <h1>My Recipes</h1>
-        <Cards />
+        <Cards items={recipes} />
       </div>
     </section>
   );
