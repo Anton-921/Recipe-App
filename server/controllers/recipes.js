420a7037c41537617f03e52f72899343323ad5c5
yeffry <yeffry45@gmail.com>
2022-10-27 15:30:44 -0400

    updated recipes controller to include logic for liking posts

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,10 +1,13 @@
 const Recipe = require("../models/recipe");
+const User = require("../models/user");
 const cloudinary = require("../middleware/cloudinary");
 
 module.exports = {
   getRecipes: async (req, res) => {
     try {
-      const recipes = await Recipe.find({}).populate("user");
+      const recipes = await Recipe.find({})
+        .populate("user", "username")
+        .populate("likes", "username");
       res.json(recipes);
     } catch (error) {
       console.log(error);
@@ -15,8 +18,6 @@ module.exports = {
     const body = req.body;
     try {
       const result = await cloudinary.uploader.upload(req.file.path);
-
-      console.log(req.session);
 
       const recipe = await Recipe.create({
         name: body.recipeName,
@@ -38,10 +39,57 @@ module.exports = {
   },
   getUserRecipes: async (req, res) => {
     try {
-      const userRecipes = await Recipe.find({ id: req.session.userId }).populate('user');
+      const userRecipes = await Recipe.find({ id: req.session.userId })
+        .populate("user", "username")
+        .populate("likes", "username");
       res.status(200).json({ recipes: userRecipes });
     } catch (error) {
       console.log(error);
+    }
+  },
+  likeRecipe: async (req, res) => {
+    const userId = req.session.userId; 
+    const id = req.params.id; 
+    const likedRecipe = await Recipe.findById(id)
+      .populate("likes", "username")
+      .populate("user");
+
+    const hasLiked = likedRecipe.likes.some(
+      (like) => userId === like._id.toHexString()
+    );
+
+    if (hasLiked) {
+      // If user has liked then we have to dislike it and remove him from the recipe
+      const updatedRecipe = await Recipe.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            likes: userId,
+          },
+          $inc: {
+            likeCount: -1,
+          },
+        },
+        { new: true }
+      ).populate("user");
+
+      res.status(200).json({ recipe: updatedRecipe });
+    } else {
+      // If user has not liked the recipe then we have to like it, add user to likes and increase likecount
+      const updatedRecipe = await Recipe.findByIdAndUpdate(
+        id,
+        {
+          $push: {
+            likes: userId,
+          },
+          $inc: {
+            likeCount: 1,
+          },
+        },
+        { new: true }
+      ).populate("user");
+
+      res.status(200).json({ recipe: updatedRecipe });
     }
   },
 };
