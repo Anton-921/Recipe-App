a87dcee08bd4b8f5b941094e5fa0509df4fee92f
yeffry <yeffry45@gmail.com>
2022-10-27 15:32:43 -0400

    add fetch request logic to handle liking posts

--- client/src/components/Cards/Card.js
+++ client/src/components/Cards/Card.js
@@ -2,8 +2,32 @@ import React from "react";
 import styles from "../Cards/Cards.module.css";
 import { AiOutlineClockCircle, AiFillFire } from "react-icons/ai";
 import { BsFillBarChartFill } from "react-icons/bs";
+import { useRecipes } from "../../context/RecipeProvider";
 
 const Cards = ({ items }) => {
+  const { recipes, setRecipes } = useRecipes();
+  const handleRecipeLike = async (id) => {
+    console.log(id);
+    const response = await fetch(
+      `http://localhost:8080/api/v1/recipes/likeRecipe/${id}`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+    const resJson = await response.json();
+    if (response.status === 200) {
+      const updatedRecipe = resJson.recipe;
+      setRecipes(
+        recipes.map((recipe) => {
+          if (recipe._id === updatedRecipe._id) {
+            return updatedRecipe;
+          }
+          return recipe;
+        })
+      );
+    }
+  };
   return (
     <div className={styles.cards}>
       {items.map((item) => (
@@ -27,16 +51,27 @@ const Cards = ({ items }) => {
                   <span>{item.difficulty}</span>
                 </li>
                 <li>
-                  <AiFillFire color="#74B8FF" size={25} />
-                  <span>{item.likes}</span>
+                  <AiFillFire
+                    color="#74B8FF"
+                    size={25}
+                    onClick={() => handleRecipeLike(item._id)}
+                    className={styles["icon-like"]}
+                  />
+                  <span>{item.likeCount}</span>
                 </li>
               </ul>
             </div>
             <div className={styles["card-description"]}>
-              <p><span>Ingredients: </span>{item.description}</p>
+              <p>
+                <span>Ingredients: </span>
+                {item.description}
+              </p>
             </div>
             <div className={styles["card-ingredients"]}>
-              <p><span>Ingredients: </span>{item.ingredients}</p>
+              <p>
+                <span>Ingredients: </span>
+                {item.ingredients}
+              </p>
             </div>
 
             <div className={styles.user}>
